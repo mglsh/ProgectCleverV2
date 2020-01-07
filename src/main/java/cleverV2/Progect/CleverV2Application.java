ca2f72c9b49144deb7de6e9b5f296e3aae2fbedd
Misha <mglsh@ukr.net>
2020-01-07 23:31:43 +0200

    Initial commit

---
+++ src/main/java/cleverV2/Progect/CleverV2Application.java
@@ -0,0 +1,13 @@
+package cleverV2.Progect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/cleverV2/Progect/CleverV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cleverV2.Progect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
