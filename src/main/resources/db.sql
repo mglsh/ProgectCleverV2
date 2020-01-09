ce36fc5bc0d105ac36759ed639345159d679f716
Misha <mglsh@ukr.net>
2020-01-10 00:46:42 +0200

    HerokuV1

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE schema2.contact (
+CREATE TABLE uy5rqv7i1n13tim4.contact (
 
 id INT NOT NULL,
 
@@ -10,7 +10,7 @@ phone VARCHAR(45) NULL,
 
 PRIMARY KEY (id));
 
-ALTER TABLE schema2.contact
+ALTER TABLE uy5rqv7i1n13tim4.contact
 
 CHANGE COLUMN id id INT(11) NOT NULL AUTO_INCREMENT ,
 
